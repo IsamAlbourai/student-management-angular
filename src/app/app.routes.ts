69fe4c918742db14357c9159b5b956a0cb545bd0
Isam <123lightningstrike@gmail.com>
2026-07-09 06:10:44 +0400

    Completed routing, reactive forms, and route guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-
+import { authGuard } from './guards/auth-guard';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Students } from './pages/students/students';
-import { About } from './pages/about/about';
-import { AddStudent } from './pages/add-student/add-student';
+
 import { StudentDetails } from './pages/student-details/student-details';
 import { EditStudent } from './pages/edit-student/edit-student';
+import { ReactiveForm } from './pages/reactive-form/reactive-form';
 
 export const routes: Routes = [
   {
@@ -31,11 +31,8 @@ export const routes: Routes = [
   },
   {
     path: 'add-student',
-    component: AddStudent
-  },
-  {
-    path: 'about',
-    component: About
+    component: ReactiveForm,
+    canActivate: [authGuard]
   },
   {
     path: '**',
---
+++ src/app/pages/reactive-form/reactive-form.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+  FormArray
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Student } from '../../models/student';
+import { StudentService } from '../../services/student.service';
+import { nameValidator } from '../../validators/name.validator';
+
+@Component({
+  selector: 'app-reactive-form',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './reactive-form.html',
+  styleUrl: './reactive-form.css'
+})
+export class ReactiveForm {
+
+  studentForm;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private studentService: StudentService,
+    private router: Router
+  ) {
+    this.studentForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3), nameValidator]],
+      age: [18, [Validators.required, Validators.min(18)]],
+      course: ['', Validators.required],
+      skills: this.formBuilder.array([])
+    });
+  }
+
+  get skills(): FormArray {
+    return this.studentForm.get('skills') as FormArray;
+  }
+
+  addSkill() {
+    this.skills.push(this.formBuilder.control('', Validators.required));
+  }
+
+  removeSkill(index: number) {
+    this.skills.removeAt(index);
+  }
+
+  saveStudent() {
+    const formValue = this.studentForm.value;
+
+    const skills = (formValue.skills || []) as string[];
+
+    const newStudent: Student = {
+      id: 0,
+      name: formValue.name || "",
+      age: Number(formValue.age),
+      course: formValue.course || "",
+      skills: skills
+    };
+
+    this.studentService.addStudent(newStudent);
+
+    this.router.navigate(['/students']);
+  }
+
+}
